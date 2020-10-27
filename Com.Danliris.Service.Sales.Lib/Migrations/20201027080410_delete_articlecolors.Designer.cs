98d10ab7adeebab6703daa642f949a07dcf36e2d
yokana777 <kawaiirockmansick@gmail.com>
2020-10-27 15:38:02 +0700

    delete article color

--- Com.Danliris.Service.Sales.Lib/Migrations/20201027080410_delete_articlecolors.Designer.cs
+++ Com.Danliris.Service.Sales.Lib/Migrations/20201027080410_delete_articlecolors.Designer.cs
@@ -4,14 +4,16 @@ using Com.Danliris.Service.Sales.Lib;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Com.Danliris.Service.Sales.Lib.Migrations
 {
     [DbContext(typeof(SalesDbContext))]
-    partial class SalesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201027080410_delete_articlecolors")]
+    partial class delete_articlecolors
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Com.Danliris.Service.Sales.Lib/SalesDbContext.cs
+++ Com.Danliris.Service.Sales.Lib/SalesDbContext.cs
@@ -46,7 +46,7 @@ namespace Com.Danliris.Service.Sales.Lib
         public DbSet<RO_Garment_SizeBreakdown> RO_Garment_SizeBreakdowns { get; set; }
         public DbSet<RO_Garment_SizeBreakdown_Detail> RO_Garment_SizeBreakdown_Details { get; set; }
         public DbSet<Rate> Rates { get; set; }
-        public DbSet<ArticleColor> ArticleColors { get; set; }
+        //public DbSet<ArticleColor> ArticleColors { get; set; }
         public DbSet<Efficiency> Efficiencies { get; set; }
         public DbSet<GarmentBookingOrder> GarmentBookingOrders { get; set; }
         public DbSet<GarmentBookingOrderItem> GarmentBookingOrderItems { get; set; }
